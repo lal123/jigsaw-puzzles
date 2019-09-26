1559bf94bb5a28f315f79d189da04c2d87ec1226
lal123 <alain23@gmail.com>
2019-09-26 14:08:47 +0200

    Introducing Hello, World!

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,18 @@ Encore
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
 
-    /*
+     .copyFiles({
+         from: './assets/img',
+
+         // optional target path, relative to the output dir
+         to: 'img/[path][name].[ext]',
+
+         // if versioning is enabled, add the file hash too
+         //to: 'images/[path][name].[hash:8].[ext]',
+
+         // only copy files matching this pattern
+         //pattern: /\.(png|jpg|jpeg)$/
+     })    /*
      * FEATURE CONFIG
      *
      * Enable & configure other features below. For a full
